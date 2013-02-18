392ddd2374250d64b0e1903d1067fa3fd04783d0
Bryan W. Reid <bwreid@gmail.com>
2013-02-18 13:20:28 -0500

    removing files

--- db/migrate/20130215003325_change_name_of_weakness_column.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNameOfWeaknessColumn < ActiveRecord::Migration
-  def change
-    rename_column :members_weaknesses, :weaknesses_id, :weakness_id
-  end
-end
--- db/migrate/20130215021258_add_classroom_id_to_members_table.rb
+++
@@ -1,5 +0,0 @@
-class AddClassroomIdToMembersTable < ActiveRecord::Migration
-  def change
-    add_column :members, :classroom_id, :integer
-  end
-end
